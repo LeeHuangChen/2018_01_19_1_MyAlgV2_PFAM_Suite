01d8ed85da2036cd322ea023bf02c6eed9964128
LeeHuangChen <lhc1@rice.edu>
2018-01-23 14:48:20 -0600

    refactor

---
+++ configurations.py
@@ -0,0 +1,6 @@
+# Sequence Generation
+FastaSeqDict = "Resources/FastaSeqDict.cpickle"
+FamToArrDictLoc = "Resources/FamToLineDict.cpickle"
+familiesFile = "Resources/familyCount.txt"
+
+fastaFolder = "Generated/0_FastaSequence"
